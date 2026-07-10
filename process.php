73ea9187838b82605a5a92af91d8b9ff83ad25ee
Sajid Muntasir <muntasirmamunsajid05@gmail.com>
2026-07-10 08:15:14 +0600

    updated

--- process.php
+++ process.php
@@ -1,33 +1,83 @@
-<?php
+
+ <?php
 
 //----1:-Conatct page form-----//
 
-if(isset($_POST['contact_name']) != ""){
-			$to = "shmai.com@gmail.com";
-			$from =  $_POST["contact_email"];
-			$message = "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Name: </strong>".$_POST["contact_name"]."<br />";
-			$message .= "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Email Address: </strong>".$_POST["contact_email"]."<br />";
-			$message .= "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Email Address: </strong>".$_POST["contact_phone"]."<br />";
-			$message .= "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Subject: </strong>".$_POST["contact_subject"]."<br />";
-			$message .= "&nbsp;&nbsp;&nbsp;&nbsp; <strong>Message: </strong>".$_POST["message"]."<br />";
-			$subject = 'Contact Enquiry';		
-			$headers = "From: ".$_POST["contact_email"]."\n";
-			$headers .= "MIME-Version: 1.0" . "\r\n";
-			$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+//   if(isset($_POST['contact_name']) != ""){
+// 			$to = "shmai.com@gmail.com";
+// 			$from =  $_POST["contact_email"];
+// 			$message = "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Name: </strong>".$_POST["contact_name"]."<br />";
+// 			$message .= "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Email Address: </strong>".$_POST["contact_email"]."<br />";
+// 			$message .= "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Email Address: </strong>".$_POST["contact_phone"]."<br />";
+// 			$message .= "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Subject: </strong>".$_POST["contact_subject"]."<br />";
+// 			$message .= "&nbsp;&nbsp;&nbsp;&nbsp; <strong>Message: </strong>".$_POST["message"]."<br />";
+// 			$subject = 'Contact Enquiry';		
+// 			$headers = "From: ".$_POST["contact_email"]."\n";
+// 			$headers .= "MIME-Version: 1.0" . "\r\n";
+// 			$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 			
-			$send =	mail($to,$subject,$message,$headers);
+// 			$send =	mail($to,$subject,$message,$headers);
 	
 	
 	
-	if($send)
-	{
-		echo "success contact";
-	}
-	else
-	{
-		echo "error";
-	}
+// 	if($send)
+// 	{
+// 		echo "success contact";
+// 	}
+// 	else
+// 	{
+// 		echo "error";
+// 	}
+// }
+<?php
+if (
+    isset($_POST['contact_name'], $_POST['contact_email'], $_POST['contact_phone'], $_POST['contact_subject'], $_POST['message']) &&
+    !empty($_POST['contact_name']) &&
+    !empty($_POST['contact_email']) &&
+    !empty($_POST['message'])
+) {
+    // FIX #2: Sanitize all inputs
+    $name    = htmlspecialchars($_POST["contact_name"],    ENT_QUOTES, 'UTF-8');
+    $phone   = htmlspecialchars($_POST["contact_phone"],   ENT_QUOTES, 'UTF-8');
+    $subject = htmlspecialchars($_POST["contact_subject"], ENT_QUOTES, 'UTF-8');
+    $msg     = htmlspecialchars($_POST["message"],         ENT_QUOTES, 'UTF-8');
+
+    // FIX #1: Validate email before using in headers
+    $from = filter_var($_POST["contact_email"], FILTER_SANITIZE_EMAIL);
+    if (!filter_var($from, FILTER_VALIDATE_EMAIL)) {
+        echo "error: invalid email";
+        exit;
+    }
+
+    $to = "sitemailtested@gmail.com"; // FIX #8: real recipient
+
+    // FIX #5: Correct label for phone field
+    $message  = "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Name:</strong> "    . $name    . "<br />";
+    $message .= "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Email:</strong> "   . $from    . "<br />";
+    $message .= "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Phone:</strong> "   . $phone   . "<br />";
+    $message .= "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Subject:</strong> " . $subject . "<br />";
+    $message .= "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Message:</strong> " . $msg     . "<br />";
+
+    $mail_subject = "Contact Enquiry: " . $subject;
+
+    // FIX #4 & #6: Consistent \r\n and UTF-8 encoding
+    $headers  = "From: " . $from . "\r\n";
+    $headers .= "Reply-To: " . $from . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+
+    $send = mail($to, $mail_subject, $message, $headers);
+
+    if ($send) {
+        echo "success contact";
+    } else {
+        echo "error";
+    }
+
+} else {
+    echo "error: missing required fields";
 }
+?>
 
 //----2:-Newsletter form-----//
 
@@ -117,5 +167,5 @@ if(isset($_POST['name']) != ""){
 
 
 ?>
-
+***//
 
